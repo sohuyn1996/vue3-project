7b122d493b87f0b1e2dd6b13cf1ec393c27bcc22
Nguyễn Thành Hậu <haunt@nitrotech.asia>
2025-03-26 14:15:42 +0700

    update master

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -14,7 +14,7 @@ const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
     <template #icon>
       <DocumentationIcon />
     </template>
-    <template #heading>Documentation Revert</template>
+    <template #heading>Documentation Revert New New</template>
 
     Vue’s
     <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
